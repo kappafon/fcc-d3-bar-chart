f6594a207eac39c28d83dd6be5a0f473b4ae27dd
kappafon <goran.rihter@gmail.com>
2020-06-21 14:51:58 +0200

    Updates

---
+++ precache-manifest.b168a3429de7fe9763d20422260100df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175a9a39fcd2c8ae4ef87ea7051e98d9",
+    "url": "/fcc-d3-bar-chart/index.html"
+  },
+  {
+    "revision": "a3c675c805d3928e62fc",
+    "url": "/fcc-d3-bar-chart/static/css/main.9f6e4d72.chunk.css"
+  },
+  {
+    "revision": "698bc3482f3f8ded780e",
+    "url": "/fcc-d3-bar-chart/static/js/2.f58e9697.chunk.js"
+  },
+  {
+    "revision": "a3c675c805d3928e62fc",
+    "url": "/fcc-d3-bar-chart/static/js/main.0e4f3861.chunk.js"
+  },
+  {
+    "revision": "3690875fd0624590719a",
+    "url": "/fcc-d3-bar-chart/static/js/runtime-main.f2a5002a.js"
+  }
+]);
